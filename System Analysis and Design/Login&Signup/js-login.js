ab4310abe1709aac7658cc7fde346eeb48f45db3
Daniel John <johnrenticruz@gmail.com>
2024-05-04 22:21:23 +0700

    ...

--- System Analysis and Design/Login&Signup/js-login.js
+++ System Analysis and Design/Login&Signup/js-login.js
@@ -15,10 +15,10 @@ loginBtn.addEventListener('click', ()=>{
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
     var username = document.getElementById("Email").value;
-    var password = document.getElementById("password").value;
+    var password = document.getElementById("Password").value;
   
     // You can replace the condition with your actual authentication logic
-    if (username === "johnrenticruz@gmail.com" && password === "password") {
+    if (username === "admin@gmail.com" && password === "1234") {
       // Redirect to dashboard or any other page
       window.location.href = "Home.html";
     } else {
@@ -27,30 +27,4 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     }
   });
   
-  // for register or Sign Up
-  document.getElementById("signupForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-  
-    // You can replace this with your actual sign-up logic
-    if (name && email && password) {
-      alert("Sign up successful!");
-      // Redirect to dashboard or any other page
-      window.location.href = "verify.html";
-    } else {
-      document.getElementById("error").innerText = "Please fill in all fields";
-      document.getElementById("error").style.display = "block";
-    }
-  });
-  
 
-  document.querySelectorAll(".social-icons").forEach(function(btn) {
-    btn.addEventListener("click", function() {
-      var provider = this.innerText.toLowerCase();
-      alert("Continue with " + provider);
-      // Implement social media login logic here
-    });
-  });
- 
