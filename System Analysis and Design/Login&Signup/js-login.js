eaa2152dbfc253f00af5e98546e220b936e1f490
Daniel John <johnrenticruz@gmail.com>
2024-05-04 18:51:55 +0700

    Make changes

--- System Analysis and Design/Login&Signup/js-login.js
+++ System Analysis and Design/Login&Signup/js-login.js
@@ -11,3 +11,19 @@ loginBtn.addEventListener('click', ()=>{
     container.classList.remove("active");
 })
 
+// for Log in JS
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var username = document.getElementById("Email").value;
+    var password = document.getElementById("password").value;
+  
+    // You can replace the condition with your actual authentication logic
+    if (username === "admin" && password === "password") {
+      // Redirect to dashboard or any other page
+      window.location.href = "Home.html";
+    } else {
+      document.getElementById("error").innerText = "Invalid username or password";
+      document.getElementById("error").style.display = "block";
+    }
+  });
+  
